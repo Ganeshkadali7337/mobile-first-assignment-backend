b1c9ecb9f4c2b605fbcc013dd9e0797221226914
Ganeshkadali7337 <ganeshkadali0@gmail.com>
2024-06-10 13:10:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const axios = require("axios");
 const cors = require("cors");
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "ganesh123ex@gmail.com",
-    pass: "nrvq kwif nivy bpxe",
-  },
-});
 
 app.use(cors());
 
@@ -42,11 +33,7 @@ const authMiddleware = (req, res, next) => {
     if (error) {
       return res.status(400).send("invalid token");
     }
-    if (payload.tokenType !== "authentication") {
-      return res.status(400).send("invalid token");
-    }
     req.userId = payload.userId;
-    req.tokenType = payload.tokenType;
     next();
   });
 };
@@ -91,7 +78,6 @@ app.post("/signin", async (req, res) => {
       } else {
         let payload = {
           userId: existed._id,
-          tokenType: "authentication",
         };
         let token = jwt.sign(payload, "ganesh");
         res.send({ token });
@@ -99,57 +85,6 @@ app.post("/signin", async (req, res) => {
     }
   } catch (error) {
     console.log(err.message);
-    res.status(500).send(error);
-  }
-});
-
-app.post("/forget-password", async (req, res) => {
-  const { gmail } = req.body;
-  try {
-    const existed = await Users.findOne({ gmail });
-    if (!existed) {
-      res.status(400).send("user not exists");
-    }
-    let payload = {
-      userId: existed._id,
-      tokenType: "resetPassword",
-    };
-    let token = jwt.sign(payload, "ganesh", { expiresIn: "30m" });
-
-    const resetLink = `http://localhost:3000/reset-password/${token}`;
-    const msg = {
-      from: "ganesh123ex@gmail.com",
-      to: gmail,
-      subject: "reset password",
-      text: `Click the link to reset your password: ${resetLink}`,
-    };
-
-    await transporter.sendMail(msg);
-
-    res.send("password reset link has been sent to your gmail");
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.post("/reset-password/:token", async (req, res) => {
-  const { token } = req.params;
-  const { newPassword } = req.body;
-  try {
-    if (!newPassword) {
-      return res.status(400).send("please provide new password");
-    }
-    const payload = jwt.verify(token, "ganesh");
-    if (!payload || payload.tokenType !== "resetPassword") {
-      return res.status(400).send("invalid token");
-    }
-    const user = await Users.findById(payload.userId);
-
-    let hashedPassword = await bcrypt.hash(newPassword, 10);
-    user.password = hashedPassword;
-    await user.save();
-    res.send("password reset was successfull");
-  } catch (error) {
     res.status(500).send(error);
   }
 });
